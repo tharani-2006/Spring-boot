7319c3746fde7318fa0fd055a76b421ed9146f55
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-11-21 07:22:38 +0530

    demo helloworld application

---
+++ HelloWorld/src/test/java/dev/tharani/HelloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tharani.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
