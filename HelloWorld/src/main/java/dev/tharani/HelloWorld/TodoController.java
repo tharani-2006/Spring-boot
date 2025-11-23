56e0e439c9669517f5408e30fc6365d3992d4c3d
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-11-23 22:00:34 +0530

    create example of pathVariable,RequestParams,RequestBody

--- HelloWorld/src/main/java/dev/tharani/HelloWorld/TodoController.java
+++ HelloWorld/src/main/java/dev/tharani/HelloWorld/TodoController.java
@@ -1,8 +1,6 @@
 package dev.tharani.HelloWorld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/todo")
@@ -14,5 +12,34 @@ public class TodoController {
     @GetMapping("/id")
     String getTodoById(){
         return "Todo with ID";
+    }
+    //Path Variable
+    @GetMapping("/{id}")
+    String getTodoWithId(@PathVariable long id){
+        return "Todo with ID";
+    }
+
+    //RequestParams
+    @GetMapping("")
+    String getTodoWithIdParam(@RequestParam long id){
+        return "Todo with ID";
+    }
+
+    //Post Mapping
+    @PostMapping("/create")
+    String createUser(@RequestBody String body){
+        return body;
+    }
+
+    //Put Mapping
+    @PutMapping("/update/{id}")
+    String updateById(@PathVariable long id){
+        return "Update by id";
+    }
+
+    //Delete Mapping
+    @DeleteMapping("/delete/{id}")
+    String DeleteById(@PathVariable long id){
+        return "Delete by id";
     }
 }
